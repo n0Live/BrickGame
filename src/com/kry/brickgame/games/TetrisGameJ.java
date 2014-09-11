e78f394dd7cd80ea221e6099302613d240828af5
n0Live <egor.krasnopolin@gmail.com>
2014-09-11 14:53:07 +0600

    Completed work on TetrisGameJ.

--- src/com/kry/brickgame/games/TetrisGameJ.java
+++ src/com/kry/brickgame/games/TetrisGameJ.java
@@ -3,6 +3,10 @@ package com.kry.brickgame.games;
 import java.util.Timer;
 import java.util.TimerTask;
 
+/**
+ * @author noLive
+ * 
+ */
 public class TetrisGameJ extends TetrisGame {
 
 	public TetrisGameJ(int speed, int level, int type) {
@@ -12,23 +16,25 @@ public class TetrisGameJ extends TetrisGame {
 	@Override
 	public void start() {
 
-		// create timer for bullets
+		// create timer for addition of lines
 		Timer addLineTimer = new Timer("AddLineTicTac", true);
 		addLineTimer.schedule(new TimerTask() {
-			int timeToAddLine = 30;
+			final int TIME_BETWEEN_ADDING_LINE = 5;
+			int time = TIME_BETWEEN_ADDING_LINE;
 
 			@Override
 			public void run() {
 				if (getStatus() == Status.Running) {
-					fireInfoChanged("-" + String.valueOf(timeToAddLine) + "-");
-					if (timeToAddLine == 0) {
-						setStatus(Status.DoSomeWork);
+					setStatus(Status.DoSomeWork);
+					fireInfoChanged("-" + String.valueOf(time) + "-");
+					if (time == 0) {
 						addLines();
-						timeToAddLine = 30;
-						setStatus(Status.Running);
+						time = TIME_BETWEEN_ADDING_LINE;
+						sleep(ANIMATION_DELAY);
 					} else {
-						timeToAddLine--;
+						time--;
 					}
+					setStatus(Status.Running);
 				}
 			}
 		}, 0, 1000);
@@ -36,7 +42,14 @@ public class TetrisGameJ extends TetrisGame {
 		super.start();
 
 		addLineTimer.cancel();
+	}
 
+	@Override
+	protected void addLines() {
+		super.addLines();
+		// the current y-coordinate lifts by one cell upward
+		if (curY < boardHeight)
+			curY++;
 	}
 
 }
