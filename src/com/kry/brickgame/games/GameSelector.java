c675f3622db888cbb07dabffd9f2b26c059df8da
n0Live <egor.krasnopolin@gmail.com>
2014-09-11 11:44:53 +0600

    Adds link to new Tetris mod "J"

--- src/com/kry/brickgame/games/GameSelector.java
+++ src/com/kry/brickgame/games/GameSelector.java
@@ -84,7 +84,7 @@ public class GameSelector extends Game {
 		}
 		try {
 			gamesList.put('J',
-					Class.forName("com.kry.brickgame.games.TetrisGame"));
+					Class.forName("com.kry.brickgame.games.TetrisGameJ"));
 		} catch (ClassNotFoundException e) {
 			gamesList.put('J', null);
 		}
