3b9f86ca7c7bcc7efbd4f848e6ad2bbcf737bc29
n0Live <egor.krasnopolin@gmail.com>
2014-09-12 13:29:30 +0600

    Added hashCode() and equals()

--- src/com/kry/brickgame/Board.java
+++ src/com/kry/brickgame/Board.java
@@ -1,5 +1,7 @@
 package com.kry.brickgame;
 
+import java.util.Arrays;
+
 /**
  * @author noLive
  * 
@@ -162,5 +164,39 @@ public class Board {
 			result.append(line).append("\n");
 		}
 		return result.toString();
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + Arrays.hashCode(board);
+		result = prime * result + height;
+		result = prime * result + width;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Board other = (Board) obj;
+		if (!Arrays.deepEquals(board, other.board)) {
+			return false;
+		}
+		if (height != other.height) {
+			return false;
+		}
+		if (width != other.width) {
+			return false;
+		}
+		return true;
 	}
 }
