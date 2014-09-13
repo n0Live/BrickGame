478b9244d22462fca7f27afedb45da7c7f3d7ce6
n0Live <egor.krasnopolin@gmail.com>
2014-09-13 23:31:49 +0600

    Completed work on TetrisGameK

--- src/com/kry/brickgame/games/TetrisGameK.java
+++ src/com/kry/brickgame/games/TetrisGameK.java
@@ -1,10 +1,63 @@
 package com.kry.brickgame.games;
 
+import com.kry.brickgame.shapes.TetrisShape;
+import com.kry.brickgame.shapes.TetrisShape.Tetrominoes;
+
 public class TetrisGameK extends TetrisGame {
 
 	public TetrisGameK(int speed, int level, int type) {
 		super(speed, level, type);
-		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	protected void processKeys() {
+		if (getStatus() == Status.None)
+			return;
+
+		if (keys.contains(KeyPressed.KeyReset)) {
+			keys.remove(KeyPressed.KeyReset);
+			ExitToMainMenu();
+			return;
+		}
+
+		if (keys.contains(KeyPressed.KeyStart)) {
+			keys.remove(KeyPressed.KeyStart);
+			pause();
+			return;
+		}
+
+		if ((getStatus() == Status.Running) && (!isFallingFinished)) {
+			if (keys.contains(KeyPressed.KeyLeft)) {
+				tryMove(curPiece, curX - 1, curY);
+				sleep(ANIMATION_DELAY * 3);
+			}
+			if (keys.contains(KeyPressed.KeyRight)) {
+				tryMove(curPiece, curX + 1, curY);
+				sleep(ANIMATION_DELAY * 3);
+			}
+			if (keys.contains(KeyPressed.KeyRotate)) {
+				// if we have the super gun
+				if (curPiece.getShape() == Tetrominoes.SuperGun) {
+					// than shoot of it
+					shoot(curX, curY + curPiece.minY());
+				} else if (curPiece.getShape() == Tetrominoes.SuperMudGun) {
+					mudShoot(curX, curY + curPiece.minY());
+				} else {
+					TetrisShape rotatedPiece = TetrisShape.getNextShape(
+							curPiece, false);
+					tryMove(rotatedPiece, curX, curY);
+				}
+				keys.remove(KeyPressed.KeyRotate);
+			}
+			if (keys.contains(KeyPressed.KeyDown)) {
+				oneLineDown();
+				sleep(ANIMATION_DELAY);
+			}
+			if (keys.contains(KeyPressed.KeyUp)) {
+				dropDown();
+				keys.remove(KeyPressed.KeyUp);
+			}
+		}
 	}
 
 }
