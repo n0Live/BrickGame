ca82762c696f554f0116d996194098c605b941df
n0Live <egor.krasnopolin@gmail.com>
2014-09-15 13:03:53 +0600

    Created Pentux splash screen.

---
+++ src/com/kry/brickgame/splashes/PentixSplash.java
@@ -0,0 +1,40 @@
+package com.kry.brickgame.splashes;
+
+/**
+ * @author noLive
+ * 
+ */
+public class PentixSplash extends Splash {
+
+	private static Cell[][][] frameTable = new Cell[][][] { {
+			// 0
+			{ E, E, E, F, F, E, E, E, E, E }, //
+			{ E, E, F, F, E, E, E, E, E, E }, //
+			{ E, E, E, F, E, E, E, E, E, E }, //
+			{ E, E, E, E, E, E, E, E, E, E }, //
+			{ E, E, E, E, E, E, E, E, E, E }, //
+			{ F, E, E, E, E, E, E, E, E, E }, //
+			{ F, F, E, E, E, F, E, F, F, F }, //
+			{ F, F, F, E, F, F, E, F, F, F } }, {
+			// 1
+			{ F, E, E, F, F, E, E, E, E, E },//
+			{ F, F, F, F, E, F, E, F, F, F },//
+			{ F, F, F, F, F, F, E, F, F, F } }, {
+			// 2
+			{ E, E, E, E, E, F, F, F, E, E }, //
+			{ E, E, E, E, E, E, F, E, E, E },//
+			{ E, E, E, E, E, E, F, E, E, E },//
+			{ E, E, E, E, E, E, E, E, E, E },//
+			{ E, E, E, E, E, E, E, E, E, E },//
+			{ F, E, E, F, F, E, E, E, E, E },//
+			{ F, F, F, F, E, F, E, F, F, F },//
+			{ F, F, F, F, F, F, E, F, F, F } }, {
+			// 3
+			{ F, E, E, F, F, F, F, F, E, E },//
+			{ F, F, F, F, E, F, F, F, F, F } } };
+
+	public PentixSplash() {
+		super(frameTable);
+	}
+
+}
