b155c2b7162451fa2ad9a31ef3628902b3593f87
n0Live <egor.krasnopolin@gmail.com>
2014-11-29 19:20:09 +0600

    Buttons color depends on background

--- src/com/kry/brickgame/UI/UIConsts.java
+++ src/com/kry/brickgame/UI/UIConsts.java
@@ -1,7 +1,12 @@
 package com.kry.brickgame.UI;
 
+import java.awt.BasicStroke;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.Stroke;
+import java.awt.event.MouseAdapter;
+
+import javax.swing.JComponent;
 
 /**
  * @author noLive
@@ -48,9 +53,19 @@ public class UIConsts {
 	 */
 	protected static final Color fullColor = new Color(40, 40, 40, 255);
 	/**
-	 * Background color
+	 * Game field background color
 	 */
 	protected static final Color bgColor = new Color(136, 153, 107);
+	/**
+	 * Device background color
+	 */
+	protected static final Color deviceBgColor = new Color(60, 60, 60);
+
+	protected static final Color lineNormalColor = getBWInverted(deviceBgColor);
+	protected static final Color lineOverColor = getEnhanced(lineNormalColor);
+
+	protected static final Stroke lineNormaStroke = new BasicStroke(2f);
+	protected static final Stroke lineOverStroke = new BasicStroke(3f);
 
 	/**
 	 * Calculate new dimension (width and height) depending on specified
@@ -74,4 +89,70 @@ public class UIConsts {
 		return new Dimension(calcWidth, calcHeight);
 	}
 
+	/**
+	 * Returns the color is inverted to a given.
+	 * 
+	 * @param c
+	 *            original color
+	 * @return inverted color
+	 */
+	protected static Color getInverted(Color c) {
+		int r = c.getRed();
+		int g = c.getGreen();
+		int b = c.getBlue();
+		int alpha = c.getAlpha();
+
+		return new Color(Math.abs(255 - r), Math.abs(255 - g),
+				Math.abs(255 - b), alpha);
+	}
+
+	/**
+	 * Returns the black or white color is inverted to a given.
+	 * 
+	 * @param c
+	 *            original color
+	 * @return inverted black&white color
+	 */
+	protected static Color getBWInverted(Color c) {
+		return (isDarkColor(c) ? Color.white : Color.black);
+	}
+
+	/**
+	 * Enhances color: bright colors become brighter, dark - darker.
+	 * 
+	 * @param c
+	 *            original color
+	 * @return enhanced color
+	 */
+	protected static Color getEnhanced(Color c) {
+		return (isDarkColor(c)) ? c.darker() : c.brighter();
+	}
+
+	/**
+	 * Reduces color: bright colors become darker, dark - brighter.
+	 * 
+	 * @param c
+	 *            original color
+	 * @return reduced color
+	 */
+	protected static Color getReduced(Color c) {
+		return (isDarkColor(c)) ? c.brighter() : c.darker();
+	}
+
+	/**
+	 * Determines if the specified color is dark
+	 * 
+	 * @param c
+	 *            specified color
+	 * @return {@code true} if that color is dark; {@code false} otherwise
+	 */
+	protected static boolean isDarkColor(Color c) {
+		int r = c.getRed();
+		int g = c.getGreen();
+		int b = c.getBlue();
+
+		// if brightness of all color components less then half of max
+		// brightness
+		return (r + g + b) < (255 * 3) / 2;
+	}
 }
