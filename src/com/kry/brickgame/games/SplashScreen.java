651855167b242de36c8ba62d744b7ea6cc5222c7
User <User@192.168.158.96>
2014-12-09 11:59:01 +0600

    Added processing of the menu buttons in SplashScreen

--- src/com/kry/brickgame/games/SplashScreen.java
+++ src/com/kry/brickgame/games/SplashScreen.java
@@ -19,14 +19,21 @@ import com.kry.brickgame.sound.SoundManager;
  */
 public class SplashScreen extends Game {
 	private static final long serialVersionUID = 6213953274430176604L;
-	
+
 	private final Music welcome = Music.welcome;
-	
+
+	/**
+	 * Whether to show a SplashScreen again?
+	 */
+	private boolean resetFlag;
+
 	public SplashScreen() {
 		super();
 		SoundManager.prepare(music, welcome);
+		resetFlag = false;
 	}
-	
+
+
 	/**
 	 * Animated walking in a spiral with inverting cells on the main board
 	 */
@@ -38,7 +45,7 @@ public class SplashScreen extends Game {
 		// y: board.height --> 0
 		int fromY = board.getHeight() - 1;
 		int toY = 0;
-		
+
 		// until it reaches the middle of the board
 		while (fromX != board.getWidth() / 2) {
 			// spiral motion with a gradually narrowing
@@ -48,7 +55,7 @@ public class SplashScreen extends Game {
 		}
 		sleep(ANIMATION_DELAY * 2);
 	}
-	
+
 	/**
 	 * Blinking "9999" on the board specified number of times
 	 * 
@@ -57,18 +64,18 @@ public class SplashScreen extends Game {
 	 */
 	protected void blinkNumbers(int repeatCount) {
 		if (repeatCount <= 0) return;
-		
+
 		for (int i = 0; i < repeatCount; i++) {
-			
+
 			if (isInterrupted()) return;
-			
+
 			clearBoard();
 			sleep(ANIMATION_DELAY * 5);
 			insertNumbers();
 			sleep(ANIMATION_DELAY * 6);
 		}
 	}
-	
+
 	/**
 	 * Animated horizontal moving and inverting cells
 	 * 
@@ -85,13 +92,13 @@ public class SplashScreen extends Game {
 		Board board = getBoard();
 		// define the direction by coordinates
 		boolean isRightDirection = (toX >= fromX);
-		
+
 		// left to right
 		if (isRightDirection) {
 			for (int i = fromX; i <= toX; i++) {
-				
+
 				if (isInterrupted()) return false;
-				
+
 				// invert cells
 				board.setCell(((board.getCell(i, y) == Cell.Empty) ? Cell.Full : Cell.Empty), i, y);
 				fireBoardChanged(board);
@@ -100,9 +107,9 @@ public class SplashScreen extends Game {
 			// right to left
 		} else {
 			for (int i = fromX; i >= toX; i--) {
-				
+
 				if (isInterrupted()) return false;
-				
+
 				// invert cells
 				board.setCell(((board.getCell(i, y) == Cell.Empty) ? Cell.Full : Cell.Empty), i, y);
 				fireBoardChanged(board);
@@ -111,16 +118,16 @@ public class SplashScreen extends Game {
 		}
 		return true;
 	}
-	
+
 	/**
 	 * Draws a "9999" on the main board
 	 */
 	private void insertNumbers() {
 		Board board = getBoard().clone();
 		BoardNumbers boardNumber = new BoardNumbers();
-		
+
 		boardNumber.setNumber(BoardNumbers.intToNumbers(9));
-		
+
 		// upper left
 		insertCellsToBoard(board, boardNumber.getBoard(), 1,
 				board.getHeight() - boardNumber.getHeight() - 1);
@@ -132,39 +139,48 @@ public class SplashScreen extends Game {
 		// lower right
 		insertCellsToBoard(board, boardNumber.getBoard(), board.getWidth() - boardNumber.getWidth()
 				- 1, 1);
-		
+
 		if (!isInterrupted()) {
 			setBoard(board);
 		}
 	}
-	
+
 	/**
 	 * Processing of key presses
 	 */
 	@Override
 	protected void processKeys() {
 		if (getStatus() == Status.None) return;
-		
+
 		if (keys.contains(KeyPressed.KeyOnOff)) {
 			keys.remove(KeyPressed.KeyOnOff);
 			quit();
 			return;
 		}
-		
+
+		if (keys.contains(KeyPressed.KeyMute)) {
+			keys.remove(KeyPressed.KeyMute);
+			setMuted(!isMuted());
+		}
+
+		if (keys.contains(KeyPressed.KeyReset)) {
+			resetFlag = true;
+		}
+
 		if (!keys.isEmpty()) {
 			setStatus(Status.None);
 		}
 	}
-	
+
 	@Override
 	public void start() {
 		setStatus(Status.DoSomeWork);
-		
+
 		playMusic(welcome);
 		sleep(ANIMATION_DELAY);
-		
+
 		insertNumbers();
-		
+
 		// Splash screen will be run in a separate thread
 		Thread splashScreenThread = new Thread(new Runnable() {
 			@Override
@@ -175,23 +191,27 @@ public class SplashScreen extends Game {
 				}
 			}
 		}, "SplashScreen");
-		
+
 		splashScreenThread.start();
-		
+
 		// by pressing any key status sets to Status.None
-		while (getStatus() != Status.None) {
+		while (getStatus() == Status.DoSomeWork) {
 			processKeys();
 		}
-		
+
 		splashScreenThread.interrupt();
 		// Waits for end of interrupting splashScreenThread
 		while (splashScreenThread.isAlive()) {
 			;// wait
 		}
-		
-		Main.setGame(Main.gameSelector.reset());
+
+		if (resetFlag) {
+			Main.setGame(new SplashScreen());
+		} else {
+			Main.setGame(Main.gameSelector.restart());
+		}
 	}
-	
+
 	/**
 	 * Animated vertical moving and inverting cells
 	 * 
@@ -208,13 +228,13 @@ public class SplashScreen extends Game {
 		Board board = getBoard();
 		// define the direction by coordinates
 		boolean isUpDirection = (toY >= fromY);
-		
+
 		// bottom to top
 		if (isUpDirection) {
 			for (int i = fromY; i <= toY; i++) {
-				
+
 				if (isInterrupted()) return false;
-				
+
 				// invert cells
 				board.setCell(((board.getCell(x, i) == Cell.Empty) ? Cell.Full : Cell.Empty), x, i);
 				fireBoardChanged(board);
@@ -223,9 +243,9 @@ public class SplashScreen extends Game {
 			// top to bottom
 		} else {
 			for (int i = fromY; i >= toY; i--) {
-				
+
 				if (isInterrupted()) return false;
-				
+
 				// invert cells
 				board.setCell(((board.getCell(x, i) == Cell.Empty) ? Cell.Full : Cell.Empty), x, i);
 				fireBoardChanged(board);
@@ -234,5 +254,5 @@ public class SplashScreen extends Game {
 		}
 		return true;
 	}
-	
+
 }
