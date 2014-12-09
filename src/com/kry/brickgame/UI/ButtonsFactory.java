42085cff69e1932cbe501e5eac7a51f0831c8951
User <User@192.168.158.96>
2014-12-09 15:48:14 +0600

    Non-serializable value is stored into a non-transient field of a serializable class [FindBugs]

--- src/com/kry/brickgame/UI/ButtonsFactory.java
+++ src/com/kry/brickgame/UI/ButtonsFactory.java
@@ -43,45 +43,45 @@ import com.kry.brickgame.games.GameConsts.KeyPressed;
 public class ButtonsFactory {
 	private static abstract class AbstractWindowButton extends JButton {
 		private static final long serialVersionUID = 7175204924783660101L;
-		
+
 		protected final int gap = 2;
 		protected boolean isMouseOver;
-		
+
 		private final MouseAdapter menuMouseAdapter = new MouseAdapter() {
 			@Override
 			public void mouseEntered(MouseEvent e) {
 				isMouseOver = true;
 				repaint();
 			}
-			
+
 			@Override
 			public void mouseExited(MouseEvent e) {
 				isMouseOver = false;
 				repaint();
 			}
-			
+
 			@Override
 			public void mouseReleased(MouseEvent e) {
 				doWhenMouseReleased(e);
 			}
 		};
-		
+
 		public AbstractWindowButton() {
 			super();
 			setFocusable(false);
 			setContentAreaFilled(false);
 			setBorderPainted(false);
-			
+
 			isMouseOver = false;
 			addMouseListener(menuMouseAdapter);
 		}
-		
+
 		abstract void doWhenMouseReleased(MouseEvent e);
 	}
-	
+
 	private static class ButtonClose extends AbstractWindowButton {
 		private static final long serialVersionUID = -7518553794423312339L;
-		
+
 		@Override
 		void doWhenMouseReleased(MouseEvent e) {
 			// if mouse released upon the component
@@ -91,35 +91,35 @@ public class ButtonsFactory {
 				frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 			}
 		}
-		
+
 		@Override
 		protected void paintComponent(Graphics g) {
 			Graphics2D g2d = (Graphics2D) g;
 			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			
+
 			// save previous graphical properties for the further restore
 			Stroke oldStroke = g2d.getStroke();
 			Paint oldPaint = g2d.getPaint();
-			
+
 			g2d.setColor(isMouseOver ? lineOverColor : lineNormalColor);
 			g2d.setStroke(isMouseOver ? lineOverStroke : lineNormaStroke);
-			
+
 			int sideLength = Math.min(getWidth(), getHeight());
-			
+
 			g2d.drawLine(gap, gap, sideLength - gap, sideLength - gap);
 			g2d.drawLine(gap, sideLength - gap, sideLength - gap, gap);
-			
+
 			// restore properties from the backup
 			g2d.setStroke(oldStroke);
 			g2d.setPaint(oldPaint);
 			super.paintComponent(g2d);
 		}
-		
+
 	}
-	
+
 	private static class ButtonMinimize extends AbstractWindowButton {
 		private static final long serialVersionUID = -7518553794423312339L;
-		
+
 		@Override
 		void doWhenMouseReleased(MouseEvent e) {
 			// if mouse released upon the component
@@ -129,30 +129,30 @@ public class ButtonsFactory {
 				frame.setState(Frame.ICONIFIED);
 			}
 		}
-		
+
 		@Override
 		protected void paintComponent(Graphics g) {
 			Graphics2D g2d = (Graphics2D) g;
 			g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-			
+
 			// save previous graphical properties for the further restore
 			Stroke oldStroke = g2d.getStroke();
 			Paint oldPaint = g2d.getPaint();
-			
+
 			g2d.setColor(isMouseOver ? lineOverColor : lineNormalColor);
 			g2d.setStroke(isMouseOver ? lineOverStroke : lineNormaStroke);
-			
+
 			int sideLength = Math.min(getWidth(), getHeight());
-			
+
 			g2d.drawLine(gap, sideLength - gap, sideLength - gap, sideLength - gap);
-			
+
 			// restore properties from the backup
 			g2d.setStroke(oldStroke);
 			g2d.setPaint(oldPaint);
 			super.paintComponent(g2d);
 		}
 	}
-	
+
 	/**
 	 * Provides the effect of pressing the button when pressing the
 	 * corresponding keyboard key
@@ -167,7 +167,7 @@ public class ButtonsFactory {
 		private static void setPressed(JButton btn) {
 			setPressed(btn, true);
 		}
-		
+
 		/**
 		 * Sets the button to pressed or unpressed.
 		 * 
@@ -183,7 +183,7 @@ public class ButtonsFactory {
 				btnMdl.setPressed(b);
 			}
 		}
-		
+
 		/**
 		 * Sets the button to unpressed state.
 		 * 
@@ -193,7 +193,7 @@ public class ButtonsFactory {
 		private static void setUnpressed(JButton btn) {
 			setPressed(btn, false);
 		}
-		
+
 		@Override
 		public boolean dispatchKeyEvent(KeyEvent e) {
 			if (e.getID() == Event.KEY_PRESS) {
@@ -210,14 +210,14 @@ public class ButtonsFactory {
 			return false;
 		}
 	}
-	
+
 	public static class GameButton extends JButton {
 		private static final long serialVersionUID = 575073369779220772L;
 		/**
 		 * Action key, with which the button is associated (read only)
 		 */
 		public final KeyPressed mappedKey;
-		
+
 		/**
 		 * The button's default icon
 		 */
@@ -230,11 +230,11 @@ public class ButtonsFactory {
 		 * The button's pressed icon
 		 */
 		private final BufferedImage pressed;
-		
+
 		/**
 		 * Changes icon size when changing the button size
 		 */
-		ComponentListener btnResizeListener = new ComponentAdapter() {
+		transient ComponentListener btnResizeListener = new ComponentAdapter() {
 			@Override
 			public void componentResized(ComponentEvent e) {
 				setIcon(getScaledIcon(normal));
@@ -242,7 +242,7 @@ public class ButtonsFactory {
 				setPressedIcon(getScaledIcon(pressed));
 			}
 		};
-		
+
 		/**
 		 * Create button, assigned to the specified key
 		 * 
@@ -252,29 +252,29 @@ public class ButtonsFactory {
 		public GameButton(KeyPressed key) {
 			super();
 			mappedKey = key;
-			
+
 			setFocusable(false);
 			setContentAreaFilled(false);
 			setBorderPainted(false);
 			setRolloverEnabled(true);
-			
+
 			// fix displaying at different LookAndFeel
 			setMargin(new Insets(0, 0, 0, 0));
 			setUI(new BasicButtonUI());
-			
+
 			setActionCommand(key.toString());
-			
+
 			normal = UIUtils.getImage("/images/btn_" + key.toString() + "_n.png");
 			hover = UIUtils.getImage("/images/btn_" + key.toString() + "_h.png");
 			pressed = UIUtils.getImage("/images/btn_" + key.toString() + "_p.png");
-			
+
 			addMouseListener(btnMouseListener);
 			addComponentListener(btnResizeListener);
-			
+
 			setMinimumSize(getPreferredSize());
 			setMaximumSize(getPreferredSize());
 		}
-		
+
 		/**
 		 * Returns a scaled version of specified image to fit in the size of the
 		 * component
@@ -288,17 +288,17 @@ public class ButtonsFactory {
 					Image.SCALE_SMOOTH));
 		}
 	}
-	
+
 	/**
 	 * Mouse listener for the buttons
 	 */
 	private static ButtonMouseListener btnMouseListener = null;
-	
+
 	/**
 	 * Map of the added buttons, assigned to the keys
 	 */
 	private static Map<KeyPressed, GameButton> buttons = null;
-	
+
 	/**
 	 * Returns button, assigned to the specified key
 	 * 
@@ -314,17 +314,17 @@ public class ButtonsFactory {
 		if (null == buttons) {
 			buttons = new HashMap<>();
 		}
-		
+
 		GameButton button = buttons.get(key);
 		// if button not created yet
 		if (null == button) {
 			button = new GameButton(key);
 			buttons.put(key, button);
 		}
-		
+
 		return button;
 	}
-	
+
 	/**
 	 * Gets standard-looking close button
 	 * 
@@ -333,7 +333,7 @@ public class ButtonsFactory {
 	public static JButton getCloseButton() {
 		return new ButtonClose();
 	}
-	
+
 	/**
 	 * Gets standard-looking minimize button
 	 * 
@@ -342,5 +342,5 @@ public class ButtonsFactory {
 	public static JButton getMinimizeButton() {
 		return new ButtonMinimize();
 	}
-	
+
 }
