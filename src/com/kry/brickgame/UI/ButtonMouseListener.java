56d78fe057c04e327dccd49a6d107d41da8f6e12
n0Live <egor.krasnopolin@gmail.com>
2014-12-24 22:48:01 +0600

    HashSet menuKeys replaced by unmodifiableList

--- src/com/kry/brickgame/UI/ButtonMouseListener.java
+++ src/com/kry/brickgame/UI/ButtonMouseListener.java
@@ -4,10 +4,10 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.WindowEvent;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -46,14 +46,14 @@ class ButtonMouseListener extends MouseAdapter {
 	/**
 	 * Menu buttons (Shutdown, Reset, Mute, Start)
 	 */
-	private static final Set<KeyPressed> menuKeys = new HashSet<>(Arrays.asList(
-			KeyPressed.KeyShutdown, KeyPressed.KeyReset, KeyPressed.KeyMute, KeyPressed.KeyStart));
+	private static final List<KeyPressed> menuKeys = Collections.unmodifiableList(Arrays.asList(
+	        KeyPressed.KeyShutdown, KeyPressed.KeyReset, KeyPressed.KeyMute, KeyPressed.KeyStart));
 	
 	/**
 	 * Threads for processing of pressed keys
 	 */
 	private final ScheduledExecutorService scheduledThreadPool = Executors
-			.newScheduledThreadPool(5);
+	        .newScheduledThreadPool(5);
 	
 	/**
 	 * Pressed keys
@@ -77,7 +77,7 @@ class ButtonMouseListener extends MouseAdapter {
 		} else {
 			// process the pressing buttons as well as pressing a key
 			ScheduledFuture<?> pressedKey = scheduledThreadPool.scheduleAtFixedRate(new Repeater(
-					key), 300, 40, TimeUnit.MILLISECONDS);
+			        key), 300, 40, TimeUnit.MILLISECONDS);
 			pressedKeys.put(key, pressedKey);
 			
 			Main.getGame().keyPressed(key);
@@ -95,7 +95,7 @@ class ButtonMouseListener extends MouseAdapter {
 			if (key == KeyPressed.KeyShutdown && e.getComponent().contains(e.getPoint())) {
 				// send closing event to parent frame
 				JFrame frame = (JFrame) SwingUtilities.getAncestorOfClass(JFrame.class,
-						e.getComponent());
+				        e.getComponent());
 				frame.dispatchEvent(new WindowEvent(frame, WindowEvent.WINDOW_CLOSING));
 			} else {
 				Main.getGame().keyReleased(key);
